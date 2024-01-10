e9c8e186714fe5e75dd40271d0cc5fec2f4c7e68
Gentaralam <mohgentaralamtp18@gmail.com>
2024-01-10 20:20:29 +0700

    Final project

---
+++ script.js
@@ -0,0 +1,58 @@
+window.addEventListener('load',()=>{
+    let allTab = document.querySelectorAll('.tab');
+    let allproject = document.querySelectorAll('.project');
+
+
+    // remove tab active
+    const removeActive =()=> allTab.forEach(tab=>{
+        tab.classList.remove('tab-active');
+    })
+
+    allTab.forEach(tab=>{
+        // add tab active
+        tab.addEventListener('click',(event)=>{
+            removeActive();
+            tab.classList.add('tab-active');
+        
+        
+            let filterName = event.target.getAttribute('data-name');
+
+            allproject.forEach(project=>{
+                let projectName = project.getAttribute('data-name');
+                if ((projectName == filterName) || filterName == 'All'){
+                    project.style.display = 'block';
+                    project.style.display = 'flex'; 
+                    project.style.gap = '0.8rem';
+                } else {
+                    project.style.display = 'none';
+                }
+            });
+        });
+    });
+});
+
+// nav scroll
+window.addEventListener('scroll',()=>{
+    let nav = document.querySelector('nav');
+
+    nav.classList.toggle('nav-scroll', window.scrollY > 0);
+});
+
+// typed text js
+document.addEventListener('DOMContentLoaded', function() {
+    let typed = new Typed('.typed-text', {
+        strings: ['Front End', 'Web Design'],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 1000,
+        loop: true,
+    });
+});
+
+// menu bar
+let menuBar = document.querySelector('.menu-bar');
+let menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click',()=>{
+    menuBar.classList.toggle('menu-none')
+});
